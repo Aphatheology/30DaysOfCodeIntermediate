4880dd2423affb66dd0ab2e670fa0b3e14177928
Aphatheology <aphatheology@gmail.com>
2020-06-01 01:45:20 +0100

    Update CSS and Javascript File

--- Day1Counter/script.js
+++ Day1Counter/script.js
@@ -1,7 +1,6 @@
 let decreaseButton = document.getElementById('decrease');
 let increaseButton = document.getElementById('increase');
 let counter = document.getElementById('counter');
-// let count = localStorage.setItem(countNum ,)
 let count = 0
 counter.innerHTML = count;
 
@@ -24,17 +23,14 @@ function checkSign(number) {
         counter.classList.remove("negative");
         counter.classList.remove("neutral");
         counter.classList.add("positive");
-        console.log(counter);
     } else if (Math.sign(number) === (-1)) {
         counter.classList.remove("positive");
         counter.classList.remove("neutral");
         counter.classList.add("negative");
-        console.log(counter);
     }
      else {
         counter.classList.remove("negative");
         counter.classList.remove("positive");
-        counter.classList.add("neutral")
-        console.log(counter);
+        counter.classList.add("neutral");
     }
 }
